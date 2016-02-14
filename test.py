ffdf02fe351d4e7bdc56c939808e273b369d0570
Gareth <me@gareth.codes>
2016-02-14 23:25:09 +1100

    Tutorial with classes

--- test.py
+++ test.py
@@ -1,13 +1,101 @@
 import urwid
 
-quit = ('q', 'Q')
+class MenuButton(urwid.Button):
+    def __init__(self, caption, callback):
+        super(MenuButton, self).__init__("")
+        urwid.connect_signal(self, 'click', callback)
+        self._w = urwid.AttrMap(urwid.SelectableIcon(
+            [u'  \N{BULLET} ', caption], 2), None, 'selected')
 
-def show_or_exit(key):
-    if key in quit:
-        raise urwid.ExitMainLoop()
-    txt.set_text(repr(key))
+class SubMenu(urwid.WidgetWrap):
+    def __init__(self, caption, choices):
+        super(SubMenu, self).__init__(MenuButton(
+            [caption, u"\N{HORIZONTAL ELLIPSIS}"], self.open_menu))
+        line = urwid.Divider(u'\N{LOWER ONE QUARTER BLOCK}')
+        listbox = urwid.ListBox(urwid.SimpleFocusListWalker([
+            urwid.AttrMap(urwid.Text([u"\n  ", caption]), 'heading'),
+            urwid.AttrMap(line, 'line'),
+            urwid.Divider()] + choices + [urwid.Divider()]))
+        self.menu = urwid.AttrMap(listbox, 'options')
 
-txt = urwid.Text(u"Hello World")
-fill = urwid.Filler(txt, 'top')
-loop = urwid.MainLoop(fill, unhandled_input=show_or_exit)
-loop.run()
+    def open_menu(self, button):
+        top.open_box(self.menu)
+
+class Choice(urwid.WidgetWrap):
+    def __init__(self, caption):
+        super(Choice, self).__init__(
+            MenuButton(caption, self.item_chosen))
+        self.caption = caption
+
+    def item_chosen(self, button):
+        response = urwid.Text([u'  You chose ', self.caption, u'\n'])
+        done = MenuButton(u'Ok', exit_program)
+        response_box = urwid.Filler(urwid.Pile([response, done]))
+        top.open_box(urwid.AttrMap(response_box, 'options'))
+
+def exit_program(key):
+    raise urwid.ExitMainLoop()
+
+menu_top = SubMenu(u'Main Menu', [
+    SubMenu(u'Applications', [
+        SubMenu(u'Accessories', [
+            Choice(u'Text Editor'),
+            Choice(u'Terminal'),
+        ]),
+    ]),
+    SubMenu(u'System', [
+        SubMenu(u'Preferences', [
+            Choice(u'Appearance'),
+            Choice(u'Background'),
+        ]),
+        Choice(u'Lock Screen'),
+    ]),
+    SubMenu(u'Radio', [
+        SubMenu(u'Gain settings', [
+            SubMenu(u'IF', [
+                Choice(u'2'),
+                Choice(u'4'),
+                Choice(u'6'),
+                Choice(u'8'),
+                Choice(u'10'),
+                Choice(u'12'),
+                Choice(u'14'),
+                Choice(u'16'),
+                Choice(u'18'),
+                Choice(u'20'),
+            ]),
+            Choice(u'BB'),
+            Choice(u'Amp'),
+            Choice(u'Test'),
+        ])
+    ])
+])
+
+palette = [
+    (None,  'light gray', 'black'),
+    ('heading', 'black', 'light gray'),
+    ('line', 'black', 'light gray'),
+    ('options', 'dark gray', 'black'),
+    ('focus heading', 'white', 'dark red'),
+    ('focus line', 'black', 'dark red'),
+    ('focus options', 'black', 'light gray'),
+    ('selected', 'white', 'dark blue')]
+focus_map = {
+    'heading': 'focus heading',
+    'options': 'focus options',
+    'line': 'focus line'}
+
+class HorizontalBoxes(urwid.Columns):
+    def __init__(self):
+        super(HorizontalBoxes, self).__init__([], dividechars=2)
+
+    def open_box(self, box):
+        if self.contents:
+            del self.contents[self.focus_position + 1:]
+        self.contents.append((urwid.AttrMap(box, 'options', focus_map),
+                              self.options('given', 24)))
+        self.focus_position = len(self.contents) - 1
+
+top = HorizontalBoxes()
+top.open_box(menu_top.menu)
+urwid.MainLoop(urwid.Filler(top, 'middle', 20), palette).run()
